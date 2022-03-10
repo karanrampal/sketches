0cda01d1c64015bcc21ea943466bb6c6df2b1f5c
Karan Rampal <karan.rampal@hm.com>
2022-03-10 12:11:07 +0100

    Fix label creation script

--- src/create_labels.py
+++ src/create_labels.py
@@ -5,7 +5,10 @@ import argparse
 import logging
 import os
 
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 from utils.utils import set_logger
 
 
@@ -13,13 +16,11 @@ def args_parser() -> argparse.Namespace:
     """Parser for command line arguments"""
     parser = argparse.ArgumentParser(description="CLI")
     parser.add_argument("-r", "--root", type=str, default="../datasets/sketches", help="Root data dir")
-    parser.add_argument("-i", "--img_path", type=str, default="images", help="Image dir")
-    parser.add_argument("-d", "--data_path", type=str, default=".Training_data_Jan2022.xlsx", help="Data file")
-    parser.add_argument("-e", "--file_ext", type=str, default=".png", help="File extension")
-    parser.add_argument("-s", "--fill_str", type=str, default="None", help="String to fill null values")
-    parser.add_argument("-l", "--log_path", type=str, default="../labels.log", help="Path of log file")
+    parser.add_argument("-i", "--img-path", type=str, default="images", help="Image dir")
+    parser.add_argument("-d", "--data-path", type=str, default="Training_data_Jan2022.xlsx", help="Data file")
+    parser.add_argument("-s", "--fill-str", type=str, default="None", help="String to fill null values")
+    parser.add_argument("-l", "--log-path", type=str, default="labels.log", help="Path of log file")
     parser.add_argument("-m", "--mode", type=str, default="mcml", help="Operation mode can be mc/mcml")
-    parser.add_argument("-o", "--out_file", type=str, default="sketches_mlmc.csv", help="Output labels file")
 
     return parser.parse_args()
 
@@ -34,6 +35,7 @@ def multi_label(args: argparse.Namespace, data_filt: pd.DataFrame) -> None:
         data_filt,
         columns=["Type", "Category", "SubCategory", "Customer Group"],
     )
+
     tmp = out.groupby("Image Id").sum()
     assert (tmp > 1).any().any() == False
     assert (tmp < 0).any().any() == False
@@ -42,33 +44,28 @@ def multi_label(args: argparse.Namespace, data_filt: pd.DataFrame) -> None:
     assert (tmp.sum(1) > 1).all() == True
     assert tmp.reset_index().shape == out.shape
 
-    out.to_csv(os.path.join(args.root, args.out_file), index=False)
-
-
-def multi_class(args: argparse.Namespace, data_filt: pd.DataFrame) -> None:
-    """Writes multi-class labels to csv file
-    Args:
-        args: Hyperparams
-        data_filt: Filtered dataframe
-    """
-    labels = pd.DataFrame(columns=["Image Id", "labels"])
-    labels["Image Id"] = data_filt["Image Id"]
-    labels["labels"] = data_filt[["Type", "Category", "SubCategory", "Customer Group"]].agg("-".join, axis=1)
-    labels["code"] = labels["labels"].astype("category").cat.codes
-
-    labels.to_csv(os.path.join(args.root, args.out_file), index=False)
+    logging.info("Writing CSV file...")
+    out_train, out_val = train_test_split(out, test_size=0.1, random_state=42)
+    fname = "_sketches_" + args.mode + ".csv"
+    out_train.to_csv(os.path.join(args.root, "train" + fname), index=False)
+    out_val.to_csv(os.path.join(args.root, "val" + fname), index=False)
 
 
 def main() -> None:
     """Main function"""
+    np.random.seed(31415)
+
     args = args_parser()
     set_logger(args.log_path)
 
+    logging.info("Reading XLSX file...")
     sketch_df = pd.read_excel(os.path.join(args.root, args.data_path))
 
+    logging.info("Removing 'Image not found' URL's...")
     fetch_data = sketch_df.groupby("Image URL").first().reset_index()
     fetch_data = fetch_data[~(fetch_data["Image URL"] == "Image not found")]
 
+    logging.info("Fixing Image Id's...")
     mask = fetch_data["Image Id"].duplicated()
     fetch_data["Image Id"] = fetch_data["Image Id"].astype("str")
     fetch_data.loc[mask, "Image Id"] += "_1"
@@ -76,20 +73,19 @@ def main() -> None:
     del_cols = ["Image URL", "Image Name", "Product Number", "Garment group", "Department Name", "Seasonold", "UniquieVal"]
     data = fetch_data.drop(del_cols, axis=1)
 
+    logging.info("Filling NAN's...")
     data_clean = data.fillna(value=args.fill_str, axis=0)
     logging.info("Numberof unique types: %d", data_clean['Type'].nunique())
     logging.info("Numberof unique category: %d", data_clean['Category'].nunique())
     logging.info("Numberof unique subcategory: %d", data_clean['SubCategory'].nunique())
     logging.info("Numberof unique customer group: %d", data_clean['Customer Group'].nunique())
 
+    logging.info("Filter data...")
     file_list = os.listdir(os.path.join(args.root, args.img_path))
     id_list = [f[:-4] for f in file_list]
     data_filt = data_clean[data_clean["Image Id"].isin(id_list)]
 
-    if args.mode == "mcml":
-        multi_label(args, data_filt)
-    elif args.mode == "mc":
-        multi_class(args, data_filt)
+    multi_label(args, data_filt)
 
 
 if __name__ == "__main__":
